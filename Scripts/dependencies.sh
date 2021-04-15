3a490751cb5e8182981f69cbfb6a38450c95f9b7
nytpu <alex@nytpu.com>
2021-04-14 22:06:08 -0600

    add --force-yes to apt commands because of expired debian key

--- Scripts/dependencies.sh
+++ Scripts/dependencies.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # install dependencies
-apt-get -y update
-apt-get -y install software-properties-common
-apt-get -y update
-apt-get -y install git xinput-calibrator xfce4-genmon-plugin awesome apt-transport-https ahoy chip-sunvox pcmanfm pocketchip-batt pocketchip-configs pocketchip-list pocketchip-load pocketchip-localdoc pocketchip-onboard pocketchip-update pocket-home pocket-wm surf vala-terminal
+apt-get -y --force-yes update
+apt-get -y --force-yes install software-properties-common
+apt-get -y --force-yes update
+apt-get -y --force-yes install git xinput-calibrator xfce4-genmon-plugin awesome apt-transport-https ahoy chip-sunvox pcmanfm pocketchip-batt pocketchip-configs pocketchip-list pocketchip-load pocketchip-localdoc pocketchip-onboard pocketchip-update pocket-home pocket-wm surf vala-terminal
--- Scripts/pockethome.sh
+++ Scripts/pockethome.sh
@@ -4,8 +4,8 @@
 echo "deb https://o-marshmallow.github.io/PocketCHIP-pocket-home/archive/ jessie main" | sudo tee /etc/apt/sources.list.d/marshmallow-pocket-chip-home.list
 apt-key adv --keyserver hkp://pool.sks-keyservers.net:11371 --recv-keys 584F7F9F
 echo -e "Package: pocket-home\nPin: version *\nPin-Priority: 1050" | sudo tee /etc/apt/preferences.d/unpin-pocket-home.pref
-apt-get update
-apt-get install pocket-home
+apt-get -y --force-yes update
+apt-get -y --force-yes install pocket-home
 mkdir /home/chip/.pocket-home/
 wget -O /home/chip/.pocket-home/config.json https://git.nytpu.com/forks/PocketDesk/plain/files/config.json
 sudo chown -R chip: .pocket-home/
