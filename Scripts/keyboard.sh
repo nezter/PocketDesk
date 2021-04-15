fc409c4fd256f732f1f83f0be1265bffa8a9e9f4
nytpu <alex@nytpu.com>
2021-04-14 22:12:57 -0600

    fix permissions on xmodmap and xinitrc

--- Scripts/keyboard.sh
+++ Scripts/keyboard.sh
@@ -3,3 +3,5 @@
 # Keyboard
 wget -O /home/chip/.Xmodmap https://git.nytpu.com/forks/PocketDesk/plain/files/.Xmodmap
 wget -O /home/chip/.xinitrc https://git.nytpu.com/forks/PocketDesk/plain/files/.xinitrc
+chown chip: /home/chip/.Xmodmap
+chown chip: /home/chip/.xinitrc
