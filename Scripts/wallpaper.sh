fc3f18d29b096ae300d1fb30d44ffc455f8ca093
nytpu <alex@nytpu.com>
2021-04-14 22:16:26 -0600

    fix .pocket-home permissions

--- Scripts/wallpaper.sh
+++ Scripts/wallpaper.sh
@@ -2,5 +2,6 @@
 
 # Wallpaper
 wget -O ~chip/.pocket-home/PocketDESK_wall_gray_480x272.png https://git.nytpu.com/forks/PocketDesk/plain/images/PocketDESK_wall_gray_480x272.png
+chown -R chip: ~chip/.pocket-home/
 # Not needed anymore
 # sed -i -e 's#"background": "4D4D4D",#"background": "/home/chip/.pocket-home/PocketDESK_wall.png",#g'  ~/.pocket-home/config.json
